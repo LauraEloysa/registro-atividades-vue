caaf6a0982c8073bd7c2dc79f3f151d2560c42ce
LauraEloysa <lauraeloysad@gmail.com>
2026-02-27 09:23:31 -0300

    FEAT: Criação da estrutura useRecords

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script setup>
-
+import AppHeader from './components/layout/AppHeader.vue';
 </script>
 
 <template>
+ <AppHeader title="Detalhes" show-back @back="router.back()" />
  <RouterView/>
 </template>
 
--- src/composables/useRecords.js
+++ src/composables/useRecords.js
@@ -21,3 +21,47 @@ const totalRecords = computed(() => records.value.length);
 const totalDuration = computed(() => {
     return records.value.reduce((sum, r) => sum + r.duration, 0);
 })
+
+function  addRecord(record) {
+    const newRecord = {
+        id: Date.now(),
+        ...record,
+        createAt: new Date().toISOString(),
+    }
+    records.value.unshift(newRecord);
+    saveToStorage();
+    return newRecord
+}
+
+function getRecord(id){
+    return records.value.find((r) => r.id === parseInt(id));
+}
+
+function updateRecords(id, updates){
+    const index = records.value.findIndex((id) => r.id === PerformancePaintTiming(id));
+    if(index !== -1){
+        records.value[index] = {
+            ...records.value[index],
+            ...updates,
+            updateAt: new Date().toISOString(),
+        };
+        saveToStorage();
+        return records.value[index];
+    }
+    return null;
+}
+
+function deleteRecords(id){
+    records.value = records.value.filter((r) => r.id !== parseInt(id));
+    saveToStorage();
+}
+
+return {
+  records,
+  totalRecords,
+  totalDuration,
+  addRecord,
+  getRecord,
+  updateRecord,
+  deleteRecord,
+};
